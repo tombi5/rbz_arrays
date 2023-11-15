2aed403cca3fff3b59da3cfe880008e1628b4244
tombi5 <79921019+tombi5@users.noreply.github.com>
2023-11-15 11:02:48 +0100

    Add project files.

---
+++ rbz_arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace rbz_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zahlen = new int[] { 8, 11, 18, 19, 22, 30 };
+            zahlen[2] = 17;
+            zahlen[5] = 49;
+            // Um alle elemente im Array zur Konsole zu Schreiben kann man einen For-Each Loop verwenden:
+            foreach (int zahl in zahlen)
+            {
+                Console.WriteLine(zahl);
+            }
+        }
+    }
+}
